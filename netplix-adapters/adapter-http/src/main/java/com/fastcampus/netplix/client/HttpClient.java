eb3e543962916c526ce948313e44df4388b13f0a
CopyPaste <sjnnj@naver.com>
2024-11-01 16:35:23 +0900

    feat: init adapter-http modules

---
+++ netplix-adapters/adapter-http/src/main/java/com/fastcampus/netplix/client/HttpClient.java
@@ -0,0 +1,29 @@
+package com.fastcampus.netplix.client;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class HttpClient {
+
+    private final RestTemplate restTemplate;
+
+    public String request(String uri, HttpMethod httpMethod, HttpHeaders headers, Map<String, Object> params) {
+        return restTemplate.exchange(
+                uri,
+                httpMethod,
+                new HttpEntity<>(headers),
+                new ParameterizedTypeReference<String>() {
+                },
+                params
+        ).getBody();
+    }
+}
---
+++ netplix-apps/app-api/src/main/java/com/fastcampus/netplix/NetplixApiApplication.java
@@ -0,0 +1,12 @@
+package com.fastcampus.netplix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetplixApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetplixApiApplication.class);
+    }
+}
